e4a9232df0d64bfb78d696b4c247714e05e53004
lividgreen <lividgreen@gmail.com>
2012-08-14 04:53:34 +0300

    removed readme out

--- lib/task/refreshRepositoriesTask.class.php
+++ lib/task/refreshRepositoriesTask.class.php
@@ -43,7 +43,6 @@ class refreshDataTask extends sfBaseTask
 
                 if ($readme) {
                     $readmeText = base64_decode($readme['content']);
-                    echo $readmeText;
 
                     $hash = md5($readmeText);
                     if ($documentation = $repo->Documentation) {
